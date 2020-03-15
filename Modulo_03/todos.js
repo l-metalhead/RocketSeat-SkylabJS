fe4982c9784e92dfbd329890d9a5a18b66f5ee24
Lucas Soares Nogueira <lucasn.metalhead@gmail.com>
2020-03-15 20:01:46 +0000

    Adiciona função para excluir item do array - Aula 05 do módulo 03.

--- Modulo_03/todos.js
+++ Modulo_03/todos.js
@@ -14,9 +14,17 @@ function renderTodos() {
     for(todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement('a');
+        var linkText = document.createTextNode('Excluir');
+        var pos = todos.indexOf(todo);  // Posicao do item no array
 
+        linkElement.setAttribute('href', '#');  // # -> Sem efeito real
+        linkElement.setAttribute('onclick', `deleteTodo(${pos})`);
+        linkElement.appendChild(linkText);
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
         listElement.appendChild(todoElement);
+
     }
 }
 
@@ -31,3 +39,8 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+}
